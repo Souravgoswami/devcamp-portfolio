dd964f62eb7500140626b8d6f985502d7284eda4
Sourav Goswami <souravgoswami@protonmail.com>
2020-04-09 21:54:51 +0530

    Created seeds file for simple data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |x|
+	Blog.create!(
+		title: "My Blog Post #{ x.next }",
+		body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+	)
+end
+puts 'Created 10 Blogs'
+
+5.times do |x|
+	Skill.create!(
+		title: "Rails #{ x.next }", percent_utilized: rand(50..100)
+	)
+end
+puts 'Created 5 Skills'
+
+9.times do |x|
+	Portfolio.create!(
+		title: "Portfolio #{ x }",
+		subtitle: "My Great Service",
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+		main_image: "https://pixabay.com/get/57e0d2414250ad14ead18d78c72a3578083edbed555575417d2b7a.jpg",
+		thumb_image: "https://pixabay.com/get/57e0d2414250ad14f1dc8460ce21327e173dd8f85254794f712b73dc914b_640.jpg"
+	)
+end
+puts "Created 9 Portfolios"
