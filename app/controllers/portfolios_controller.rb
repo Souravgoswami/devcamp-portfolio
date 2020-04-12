c813e0312213f642501f24cb9c075968e312336a
Sourav Goswami <souravgoswami@protonmail.com>
2020-04-13 04:19:44 +0530

    Integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,21 @@ class PortfoliosController < ApplicationController
 	def index
 		@portfolio_items = Portfolio.all
 	end
+
+	def new
+		@portfolio_item = Portfolio.new
+	end
+
+	def create
+		@portfolio = Portfolio.new(params.require(:portfolio).permit(:title, :body))
+
+		respond_to do |fmt|
+			if @portfolio.save
+				fmt.html { redirect_to @portfolio, notice: 'Your post is now live!' }
+				# format.html { redirect_to portfolios_path, notice: 'Your post is now live!' }
+			else
+				fmt.html { render :new }
+			end
+		end
+	end
 end
