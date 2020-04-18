1ae9daef0f5f43cda85f41ce88f9730d1f9a8f47
Sourav Goswami <souravgoswami@protonmail.com>
2020-04-18 17:17:25 +0530

    Implemented concern for managing image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+	include Placeholder
 	validates_presence_of :title, :body, :main_image, :thumb_image
 	after_initialize :set_defaults
 
@@ -13,6 +14,7 @@ class Portfolio < ApplicationRecord
 	)
 
 	def set_defaults
+		# self.main_image ||= Placeholder.image_generator(height: 640, width: 480)
 		self.main_image ||= "https://i.imgur.com/vQfNBFJ.gif".freeze
 		self.thumb_image ||= "https://i.imgur.com/DXOkOtm.jpg".freeze
 	end
