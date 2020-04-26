90e849b15db2a2bc540a279f3c4403d2390f0729
Sourav Goswami <souravgoswami@protonmail.com>
2020-04-26 14:15:31 +0530

    Completed authentication with virtual attributes (attr)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_presence_of :name
+
+  def first_name
+    self.name.to_s.split(' ')[0].to_s
+  end
+
+  def last_name
+    self.name.to_s.split(' ')[-1].to_s
+  end
 end
