592e6a08c2aa25a83558f2a4000ba7647aecfa33
Sourav Goswami <souravgoswami@protonmail.com>
2020-05-01 04:56:12 +0530

    Implemented content tag helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,15 @@ module ApplicationHelper
 			link_to("Login", new_user_session_path)
 		end
 	end
+
+	def sample_helper
+		content_tag(:div, "My Content Tag Helper", class: 'my-content-tag-helper-class')
+	end
+
+	def source_helper(layout_name)
+		unless session[:source].to_s.strip.freeze.empty?
+			greeting =  "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout!".freeze
+  			content_tag(:p, greeting, class: 'source-greeting')
+		end
+	end
 end
