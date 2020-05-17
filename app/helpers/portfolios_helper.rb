a549a0cb540fcc9d2bfaef91770218e7958bc14c
Sourav Goswami <souravgoswami@protonmail.com>
2020-05-17 16:30:32 +0530

    Implemented partials for navigation, footer, and jumbotron for the portfolio layout

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,11 @@
 module PortfoliosHelper
+	def login_helper(style = '')
+		unless current_user.is_a? GuestUser
+			link_to "Logout", destroy_user_session_path, method: :delete, class: style
+		else
+			link_to("Sign Up", new_user_registration_path, class: style) +
+			' | '.freeze.html_safe +
+			link_to("Login", new_user_session_path, class: style)
+		end
+	end
 end
