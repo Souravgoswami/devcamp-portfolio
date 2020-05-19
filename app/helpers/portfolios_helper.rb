315b2bc00025441ce605eb11255a31f7669a237d
Sourav Goswami <souravgoswami@protonmail.com>
2020-05-20 00:48:02 +0530

    Updated form styles for portfolios

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -4,7 +4,7 @@ module PortfoliosHelper
 			link_to "Logout", destroy_user_session_path, method: :delete, class: style
 		else
 			link_to("Sign Up", new_user_registration_path, class: style) +
-			' | '.freeze.html_safe +
+			'<span class="my-2">|</span>'.freeze.html_safe +
 			link_to("Login", new_user_session_path, class: style)
 		end
 	end
